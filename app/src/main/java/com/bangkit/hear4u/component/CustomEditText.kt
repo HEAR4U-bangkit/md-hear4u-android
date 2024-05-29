8688d892498b257516e65cfe067cdf8a30996d5c
KILABID <aldimurad@gmail.com>
2024-05-29 22:35:39 +0700

    Add register page
    - add font plus sans jakarta
    - add custom edit text

---
+++ app/src/main/java/com/bangkit/hear4u/component/CustomEditText.kt
@@ -0,0 +1,54 @@
+package com.bangkit.hear4u.component
+
+import android.content.Context
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import com.bangkit.hear4u.R
+
+class CustomEditText @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
+    init{
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+                val currentInputType = inputType
+
+                when (currentInputType){
+                    InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS or InputType.TYPE_CLASS_TEXT ->{
+                        val isValid = android.util.Patterns.EMAIL_ADDRESS.matcher(input).matches()
+                        if (!isValid){
+                            setError("Email invalid", null)
+                        }
+                    }
+                    InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT ->{
+                        if (input.length < 8){
+                            setError("Password must be at least 8 characters", null)
+                        } else if(input.contains(" ")){
+                            setError("Password cannot contain spaces", null)
+                        }
+                    }
+                    else -> {
+                        error = null
+                    }
+                }
+                if (error != null){
+                    setBackgroundResource(R.drawable.edit_text_background_error)
+                } else {
+                    setBackgroundResource(R.drawable.edit_text_background)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+    }
+}
---
+++ app/src/main/java/com/bangkit/hear4u/ui/register/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.bangkit.hear4u.ui.register
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bangkit.hear4u.R
+import com.bangkit.hear4u.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val username = binding.edRegisterUsername.text.toString()
+        val email = binding.edRegisterEmail.text.toString()
+        val password = binding.edRegisterPassword.text.toString()
+
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
